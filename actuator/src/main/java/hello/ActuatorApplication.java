bf5df78e0f42f3ef378930dabb57215e49792948
yamsroun <yamsroun@gmail.com>
2023-05-01 18:34:52 +0900

    8. 액츄에이터 - HTTP 요청 응답 기록

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,7 +1,10 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
@@ -10,4 +13,8 @@ public class ActuatorApplication {
         SpringApplication.run(ActuatorApplication.class, args);
     }
 
+    @Bean
+    public HttpExchangeRepository httpExchangeRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 }
