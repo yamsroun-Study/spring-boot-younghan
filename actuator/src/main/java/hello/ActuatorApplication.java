66c19ae8035532308f9653af34d7a9b3f8bcea4b
yamsroun <yamsroun@gmail.com>
2023-05-25 00:19:58 +0900

    10. 모니터링 메트릭 활용 - 메트릭 등록3 - Timer

--- actuator/src/main/java/hello/ActuatorApplication.java
+++ actuator/src/main/java/hello/ActuatorApplication.java
@@ -1,6 +1,6 @@
 package hello;
 
-import hello.order.v2.OrderConfigV2;
+import hello.order.v3.OrderConfigV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.Import;
 
 //@Import(OrderConfigV0.class)
 //@Import(OrderConfigV1.class)
-@Import(OrderConfigV2.class)
+//@Import(OrderConfigV2.class)
+@Import(OrderConfigV3.class)
 @SpringBootApplication(scanBasePackages = "hello.controller")
 public class ActuatorApplication {
 
